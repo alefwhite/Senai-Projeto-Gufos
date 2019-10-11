848ca215d438a19561a16ebd57cee6bc4048f61e
FIC <FIC@info.local>
2019-10-11 15:50:41 -0300

    Script DDL e DML do Projeto Gufos

---
+++ Banco_De_Dados/Script_Banco/Gufos_DML.sql
@@ -0,0 +1,37 @@
+USE Gufos;
+
+/*** Inserindo os dados nas tabelas do projeto Gufos ***/
+
+-- Tabela Tipo_Usuario
+INSERT INTO Tipo_usuario(Titulo) 
+VALUES('Administrador'),
+('Aluno'); 
+
+-- Tabela Usuario
+INSERT INTO Usuario(Nome, Email, Senha, Tipo_usuario_id) 
+VALUES('Administrador', 'adm@adm.com', '123', 1),
+('Ariel', 'ariel@gmail.com', '123', 2);
+
+-- Tabela Localizacao
+INSERT INTO Localizacao(CNPJ, Razao_social, Endereco) 
+VALUES('84350569000177', 'Escola SENAI de Informática', 'Alameda Barão de Limeira, 539 - Santa Cecilia, São Paulo - SP, 01202-001');
+
+-- Tabela Categoria
+INSERT INTO Categoria(Titulo) 
+VALUES('Desenvolvimento de Sistemas'),
+('HTML e CSS'),('Marketing'),('Data Sciense');
+
+
+-- Tabela Evento
+INSERT INTO Evento(Titulo, Categoria_id, Acesso_livre, Data_evento, Localizacao_id) 
+VALUES('C#', 2, 0, '2019-08-07 18:00:00', 1),
+('Estrutura Semântica', 2, 1, GETDATE(), 1);
+
+-- Tabela de Presenca
+INSERT INTO Presenca(Evento_id, Usuario_id, Presenca_status) 
+VALUES(1, 2, ),(2, 2, );
+
+/*** Inserindo os dados nas tabelas do projeto Gufos ***/
+
+
+SELECT * FROM Evento
